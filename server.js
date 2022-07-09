d417811af8a257c562fa018a1cf7a71a5032cb36
Juan Ramos <eramos@coppel.com>
2022-07-09 14:44:30 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const compression = require('compression');
+const logger = require('morgan');
+const app = express();
+const http = require('http');
+const path = require('path');
+const port = 8000;
+app.use(compression());
+app.use(logger('dev'));
+
+// Use the whole root as static files to be able to serve the html file and
+// the build folder
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, '/')));
+// Send html on '/'path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, + '/index.html'));
+})
+// Create the server and listen on port
+http.createServer(app).listen(app.get('port'), () => {
+    console.log(`Server running on localhost:${app.get('port')}`);
+});
